ea50e8602e75adeca962159aa9b777c12b707168
AbiNabat <154244797+AbiNabat@users.noreply.github.com>
2024-01-30 02:30:06 +0300

    footer

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,18 @@ import HomeHeader from "@/components/HomeHeader";
 import Image from "next/image";
 import Logo from '../../public/logo.png'
 import HomeSearch from "@/components/HomeSearch";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
    <>
        <HomeHeader/>
         <div className='flex flex-col items-center mt-24'>
-            <Image src={Logo} alt={'logo'} width={300} height={300}   />
+            <Image src={Logo} alt={'logo'} width={300} height={300} priority style={{width: 'auto', height:'auto'}}  />
         </div>
 
        <HomeSearch/>
+
+       <Footer/>
 
    </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return(
+        <>
+        <footer className={'absolute bottom-0 text-sm text-gray-500 bg-[#f2f2f2] w-full'}>
+            <div className={'flex'}></div>
+        </footer>
+        </>
+    )
+}
